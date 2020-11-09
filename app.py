0cd72b2625028b2cde2034567e6483747069cb29
Drew Polstra <dibujaron@gmail.com>
2020-11-08 20:43:16 -0500

    Improved login stuff

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import configparser
+import time
 from flask import Flask, g, session, redirect, request, make_response, jsonify, render_template
 from requests_oauthlib import OAuth2Session
 
@@ -8,7 +9,8 @@ config.read('config.ini')
 OAUTH2_CLIENT_ID = config['OAUTH2']['ClientID']
 OAUTH2_CLIENT_SECRET = config['OAUTH2']['ClientSecret']
 OAUTH2_REDIRECT_URI = 'http://distant-horizon.io/authresult'
-
+LOGIN_EXPIRY = config.get('LOGIN', 'Timeout', 5.0)
+SERVER_URL = config.get('SERVERS', 'Address', 'ws://distant-horizon.io:25611/ws/')
 API_BASE_URL = 'https://discordapp.com/api'
 AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
 TOKEN_URL = API_BASE_URL + '/oauth2/token'
@@ -17,6 +19,8 @@ app = Flask(__name__, template_folder='', static_folder='', static_url_path='')
 application = app #for passenger wsgi
 app.debug = True
 app.config['SECRET_KEY'] = OAUTH2_CLIENT_SECRET
+
+pending_logins = {}
 
 if 'http://' in OAUTH2_REDIRECT_URI:
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
@@ -55,7 +59,7 @@ def quick_play():
     
 @app.route('/authenticate')
 def request_auth():
-    if 'discord_token' in request.cookies:
+    if session['oauth2_token']:
         session['auth_choice_made'] = True
         return redirect('./')
     discord = make_session(scope=['identify'])
@@ -77,7 +81,6 @@ def auth_result():
     session['oauth2_token'] = token
     session['auth_choice_made'] = True
     response = make_response(redirect('./'))
-    response.set_cookie('discord_token', str(token))
     return response
 
 @app.route('/controls')
@@ -94,6 +97,46 @@ def me():
     user = discord.get(API_BASE_URL + '/users/@me').json()
     return jsonify(user=user)
 
-
+@app.route('/client_start_login')
+def client_begin_login():
+    discord = make_session(token=session.get('oauth2_token'))
+    user = discord.get(API_BASE_URL + '/users/@me').json()
+    client_key = generate_login_key()
+    if client_key:
+        pending_logins[client_key] = {
+            user_info: user,
+            expiry: time.time() + LOGIN_EXPIRY
+        }        
+        return jsonify(logged_in=True, user=user, client_key=client_key, server_address=get_server_address())
+    else:
+        return jsonify(logged_in=False, server_address=get_server_address())
+        
+@app.route('/server_check_login/<client_key>')
+def server_check_login(client_key):
+    clean_pending_logins()
+    val = pending_logins[client_key]
+    if val:
+        user_data = val.user_info
+        return jsonify(found=True, user=user_data)
+    else:
+        return jsonify(found=False)
+        
+def get_server_address():
+    return SERVER_URL
+    
+def clean_pending_logins():
+    current_time = time.time()
+    for key in pending_logins:
+        val = pending_logins[key]
+        if val.expiry < current_time:
+            del pending_logins[key]
+            
+def generate_login_key():
+    tok = session.get('oauth2_token')
+    if tok:
+        return hash(tok)
+    else:
+        return None
+        
 if __name__ == '__main__':
     app.run()
