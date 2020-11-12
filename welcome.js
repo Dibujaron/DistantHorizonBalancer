78916f40592ad473df082d7f48a1225e4950e281
Drew Polstra <dibujaron@gmail.com>
2020-11-11 19:06:54 -0500

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -12,9 +12,10 @@ function checkReload() {
             const cached_build_time = getCookie("buildTime");
             console.log("server last build time is " + server_build_time + ", cached build time is " + cached_build_time); 
             if(!cached_build_time || server_build_time > cached_build_time){
-                console.log("cached client is out of date, reloading.");
+                console.log("cached client is out of date, clearing cache.");
                 setCookie("buildTime", server_build_time);
-                location.reload(true);
+                localStorage.clear()
+                cache.delete('/')
             } else {
                 console.log("cached client is up to date, no reload required.")
             }
